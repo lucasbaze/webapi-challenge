6cd78f7b72d4f9c58221943c35acf404af768757
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-04 10:29:05 -0500

    add other middleware and initialize app

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const server = express();
 
-Sing along:
+//middleware
+const morgan = require('morgan');
+server.use(morgan('tiny'));
 
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
-in every line there may be trouble, but if you worry you make it double, don't worry, be happy
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, be happy
+const helmet = require('helmet');
+server.use(helmet());
 
-Go code!
-*/
+const cors = require('cors');
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+let PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+    `Application running on ${PORT}`;
+});
