71004a4ad258ce0fe36f0b9d75486147aa726a6e
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-04 11:09:52 -0500

    added get actions, get by id, insert, and update handlers, and validate project and post id

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -1,8 +1,73 @@
 const router = require('express').Router();
 const db = require('../data/helpers/actionModel');
+const projectDB = require('../data/helpers/projectModel');
 
-router.get('/', (req, res) => {
-    res.send('Inside the action router');
+//
+//Get All actions
+router.get('/', async (req, res) => {
+    let actions = await db.get();
+    if (!actions) {
+        res.status(404).json({
+            message: 'There are no actions available at this time',
+        });
+    }
+    res.status(200).json(actions);
 });
+
+//
+//Get action by ID
+router.get('/:id', validateActionId, (req, res) => {
+    res.status(200).json(req.action);
+});
+
+//
+//Insert new action
+router.post('/', validateProjectId, async (req, res) => {
+    let action = req.body;
+    let createdAction = await db.insert(action);
+    if (!createdAction || createdAction == null) {
+        res.status(500).json({
+            message: 'There was an error saving the action',
+        });
+    }
+    res.status(200).json(createdAction);
+});
+
+//
+//update an action
+router.put('/:id', validateActionId, validateProjectId, async (req, res) => {
+    let action = req.body;
+    let { id } = req.params;
+
+    let updatedAction = await db.update(id, action);
+    if (!updatedAction || updatedAction == null) {
+        res.status(500).json({
+            message: 'There was an error updating the action',
+        });
+    }
+    res.status(200).json(updatedAction);
+});
+
+//
+//middleware
+async function validateActionId(req, res, next) {
+    let { id } = req.params;
+    let action = await db.get(id);
+    if (!action || action == null) {
+        next('Action does not exist');
+    }
+    req.action = action;
+    next();
+}
+async function validateProjectId(req, res, next) {
+    let action = req.body;
+    let id = action.project_id;
+
+    let project = await projectDB.get(id);
+    if (!project || project == null) {
+        next('No project exists with that ID');
+    }
+    next();
+}
 
 module.exports = router;
