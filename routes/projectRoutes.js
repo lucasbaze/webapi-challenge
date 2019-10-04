799e6f2e02802afea41fcb2e660dfedf037a2e51
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-04 11:35:17 -0500

    added insert project handler"

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -14,6 +14,28 @@ router.get('/', async (req, res) => {
 });
 
 //
+//Get project by ID
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.project);
+});
+
+//
+//Insert new project
+router.post('/', async (req, res) => {
+    let project = req.body;
+    let createdProject = await db.insert(project);
+    if (!createdProject || createdProject == null) {
+        res.status(500).json({
+            message: 'There was an error saving the project',
+        });
+    }
+    res.status(200).json({
+        message: 'Project saved successfully',
+        project: createdProject,
+    });
+});
+
+//
 //middleware
 async function validateProjectId(req, res, next) {
     let { id } = req.params;
@@ -22,6 +44,7 @@ async function validateProjectId(req, res, next) {
     if (!project || project == null) {
         next('No project exists with that ID');
     }
+    req.project = project;
     next();
 }
 
